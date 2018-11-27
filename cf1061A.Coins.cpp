d9e3c61e5d3c3e220c433d34ab5abd95aee48879
ccccchy <495237417@qq.com>
2018-11-27 10:12:27 +0800

    2018年11月27日(星期二)

---
+++ cf1061A.Coins.cpp
@@ -0,0 +1,36 @@
+#include<cstdio>
+#include<cmath>
+#include<cstring>
+#include<string>
+#include<stack>
+#include<map>
+#include<set>
+#include<queue>
+#include<vector>
+#include<iostream>
+#include<algorithm>        		//    ____   _   _  __   __
+using namespace std;            //   / ___| | | | | \ \ / /
+#define ll long long            //  | |     | |_| |  \ V /
+const ll INF = 0x3f3f3f3f;      //  | |___  |  _  |   | |
+const ll N   = 1e5+5;           //   \____| |_| |_|   |_|
+const ll MOD = 1e9+7;
+ll read(){
+    ll x=0,f=1;char ch=getchar();
+    while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}
+    while(ch>='0'&&ch<='9'){x=x*10+ch-'0';ch=getchar();}
+    return x*f;
+}
+
+ll n,s;
+ll ans;
+
+int main(){
+	n = read(), s = read();
+	while(s){
+		ll t = s / n;
+		ans += t;
+		s -= t * n;
+		n--;
+	}
+	cout << ans << endl;
+}
